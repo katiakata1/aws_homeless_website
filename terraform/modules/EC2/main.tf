a18a8e47983a8149b175df4e153baaa555f7eaba
Jekaterina Macilevic <jekaterinam@Jekaterinas-MacBook-Pro.local>
2024-04-28 21:51:57 +0100

    test

--- terraform/modules/EC2/main.tf
+++ terraform/modules/EC2/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "ec2" {
-  ami           = "ami-0c55b159cbfafe1f0"
+  ami           = "ami-053a617c6207ecc7b"
   instance_type = "t2.micro"  # Free Tier eligible instance type
 
   tags = {
